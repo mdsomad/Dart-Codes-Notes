7511831eda701e9b2a29ac228a6013479909e89a
Md Somad <mdsomad10@gmail.com>
2023-05-30 21:04:52 +0530

    shuffle Random complete

--- List_Shuffle()_Method.dart
+++ List_Shuffle()_Method.dart
@@ -1,13 +1,8 @@
+void main(){
 
-# Dart language 
-
-
-### List shuffle() Method explained
-
-```sh
   List<int> numList = [1,2,3,4,5,6,7,8,9];
   
-  //* Random shuffle list
+  //*  Random shuffle list
   numList.shuffle();
   
   print(numList); 
@@ -18,4 +13,4 @@
   //* numList Result this --> [1, 8, 5, 9, 4, 3, 2, 7, 6]   call 2
   //* numList Result this --> [7, 6, 1, 5, 9, 8, 3, 4, 2]   call 2
 
-  ```
+}
