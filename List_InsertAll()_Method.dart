80b4ab4875138e783f43376258124c86219f85d9
Md Somad <mdsomad10@gmail.com>
2023-05-31 16:38:06 +0530

    insertAll complete

--- List_InsertAll()_Method.dart
+++ List_InsertAll()_Method.dart
@@ -1,18 +1,4 @@
-
-# Dart language 
-
-
- ### insertAll
-
- Inserts all objects of iterable at position index in this list.
- 
- Multiple element add
-
-
-
-### List insertAll() Method explained
-
-```sh
+void main(){
 
 var numList = [1,2,3,4,5];
 
@@ -27,5 +13,9 @@ print(numList);
 
  
 //* numList result this --> [1, 2, 100, 300, 66, 85, 3, 4, 5]
+ 
 
-  ```
+
+
+
+}
